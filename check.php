bb1e8f3f87e549c2870822158e601761300b6e5b
Davide Ramondetti <github@ramondettidavide.com>
2017-02-03 17:54:35 +0100

    ISONLINE V1 BETA

---
+++ check.php
@@ -0,0 +1,26 @@
+<?php
+If (!$_POST['url'] and !$_GET['url']) {
+  Exit("Missing the URL, send it in POST or GET method");
+}
+
+If (!$_POST['protocol'] and !$_GET['protocol']) {
+  Exit("Protocol is missing. Please send it in POST or GET method");
+}
+
+If ($_GET['url']) {
+  $url = $_GET['url'];
+}
+
+If ($_POST['url']) {
+  $url = $_POST['url'];
+}
+
+If ($_POST['protocol']) {
+  $protocol = $_POST['protocol'];
+}
+
+If ($_GET['protocol']) {
+  $protocol = $_GET['protocol'];
+}
+
+$serverip = "isonline.ramondettidavide.com"; # HERE MUST GO THE SERVER IP
