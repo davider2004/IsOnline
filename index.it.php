2dc529877f7ed3ceb4c401abc51e6602e63e4ba4
Davide Ramondetti <github@ramondettidavide.com>
2017-02-03 18:22:05 +0100

    ISONLINE V1 BETA

---
+++ index.it.php
@@ -0,0 +1,56 @@
+<!doctype html>
+<html>
+<head>
+    <title>IsOnline ? The best website online status checker / RamondettiDavide</title>
+
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-type" content="text/html; charset=utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <style type="text/css">
+    body {
+        background-color: #f0f0f2;
+        margin: 0;
+        padding: 0;
+        font-family: "Open Sans", "Helvetica Neue", Helvetica, Arial, sans-serif;
+        
+    }
+    div {
+        width: 600px;
+        margin: 5em auto;
+        padding: 50px;
+        background-color: #fff;
+        border-radius: 1em;
+    }
+    a:link, a:visited {
+        color: #38488f;
+        text-decoration: none;
+    }
+    @media (max-width: 700px) {
+        body {
+            background-color: #fff;
+        }
+        div {
+            width: auto;
+            margin: 0 auto;
+            border-radius: 0;
+            padding: 1em;
+        }
+    }
+    </style>    
+</head>
+
+<body>
+<div>
+    <h1>IsOnline - The best website online status checker</h1>
+    <p><small>by RamondettiDavide</small></p>
+    <p></p>
+    <form method="POST" action="http://isonline.ramondettidavide.com/initialize.php">
+      <input type="Hidden" hidden="true" name="lang" id="lang" value="it" readonly>
+      <p>URL (senza HTTP/HTTPS): <input type="text" name="url" id="url" placeholder="ramondettidavide.com">:80 <input type="submit" value="Inizia il test!"></p>
+      <p>Protocollo: <input type="text" name="protocol" id="protocol" placeholder="https"> (PUT LIKE "https")</p>
+    </form>
+    <p></p>
+    <p><i>&copy; 2017 <a href="https://ramondettidavide.com/" target="_blank">RamondettiDavide</a>, all rights riserved</i></p>
+</div>
+</body>
+</html>
