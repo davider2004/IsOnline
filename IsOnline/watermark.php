7d7c56f701f82056354144000de08cd4fd615c02
Davide Ramondetti <github@ramondettidavide.com>
2017-02-04 10:40:03 +0100

    ISONLINE V2.0
    Code was upgraded for PHP 7.1.1 and added a watermark linking to my personal Github account.

---
+++ IsOnline/watermark.php
@@ -0,0 +1,2 @@
+<a href="https://github.com/davider2004/IsOnline"><img src="https://cdn1.iconfinder.com/data/icons/logotypes/32/github-128.png" alt="View on Github"></a><br>
+<a href="index.php">Test another website!</a>
