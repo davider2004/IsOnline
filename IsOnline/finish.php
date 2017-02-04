45100c45135f9f30d2899895301adf2d8bd95feb
Davide Ramondetti <github@ramondettidavide.com>
2017-02-04 10:44:54 +0100

    ISONLINE V2
    Added support for PHP 7.1.1 and watermark

--- IsOnline/finish.php
+++ IsOnline/finish.php
@@ -2,10 +2,10 @@
 Include("check.php");
 
 $url = $_GET['url'];
-$ping = $_GET['ping'];
+//$ping = $_GET['ping']; // DISABLED
 $fsocket = $_GET['fsocket'];
-$curl = $_GET['curl'];
-$visit = $_GET['visit'];
+$curl = $_GET['curl']; 
+//$visit = $_GET['visit']; // DISABLED
 $socket = $_GET['socket'];
 $ex1 = "no";
 $ex2 = "ok";
@@ -14,6 +14,34 @@ $result = array(
            "ok" => "<font color=green><b>OK</b></font>",
            "no" => "<font color=red><b>FAILED</b></font>"
 );
+
+if ($fsocket == "ok") {
+           $online = 1;
+}elseif ($fsocket == "no"){
+           $offline = 1;
+}
+
+if ($curl == "ok") {
+           $online = $online+1;
+}elseif ($curl == "no"){
+           $offline = $offline+1;
+}
+
+if ($socket == "ok"){
+           $online = $online+1;
+}elseif ($socket == "no"){
+           $offline = $offline+1;
+}
+
+if ($offline > $online) {
+           $res11 = "offline";
+}elseif ($online > $offline){
+           $res11 = "online";
+}
+
+$results1 = array(
+           "offline" => "<font color=red><b>OFFLINE</b></font>";
+           ""
 ?>
 
 <script type="text/javascript">
@@ -27,13 +55,14 @@ b="Excuse me, but I do not found any code for VISIT using PHP"
 <h2>TEST FINISHED!</h2> 
 <br>
 Results for <b><?php print $protocol; ?>://<?php print $url; ?></b>: <br><br>
-Ping Test: <font color="orange"><b>TEST DISABLED (<a href="#!showHelp=291853" onClick="alert(a)">i</a>)</font></b><br>
 FSocket Test: <?php print $result[$fsocket]; ?><br>
 CURL Test: <?php print $result[$curl]; ?><br>
-Visit Test: <font color="orange"><b>TEST DISABLED (<a href="#!showHelp=29184" onClick="alert(b)">i</a>)</font></b><br>
-Socket Test: <?php print $result[$socket]; ?><br><br>
+Socket Test: <?php print $result[$socket]; ?>
+Ping Test: <font color="orange"><b>TEST DISABLED (<a href="#!showHelp=291853" onClick="alert(a)">i</a>)</font></b><br>
+Visit Test: <font color="orange"><b>TEST DISABLED (<a href="#!showHelp=29184" onClick="alert(b)">i</a>)</font></b><br><br>
 
-<br><font color="Red"><b>REMEMBER! The test of Ping,FSocket and Visit can fail if you had a DDoS Protection (like CloudFlare)</b></font><br><br>
-<b>If there are more <?php print $result[$ex1]; ?> is probability this website is <font color="red">OFFLINE</font>, else, if there are more <?php print $result[$ex2]; ?> is probability this website is <font color="green">ONLINE</font>.</b><br><br>
+<h1>RESULT: Your website is <?php print $def; ?></h1>
+<br><br>
 
-Thanks for using this software and bye! (Excuse me for my English, but English isn't my native language)
+Thanks for using this software and bye! (Excuse me for my English, but English isn't my native language)<br>
+<?php watermark(); ?>
